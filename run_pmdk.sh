53b50ca9f0600ae6d3c257a10e5b087f8d8abba4
Ron Gatenio <ron.gatenio@gmail.com>
2023-07-30 00:17:29 +0300

    updated script

--- run_pmdk.sh
+++ run_pmdk.sh
@@ -3,8 +3,15 @@
 PM_FILE="/tmp/btree_data"
 INPUT_FILE="sample_input.txt"
 NKEYS=1000
+NKEYS=11
 NTHREADS=4
 
-rm -f $PM_FILE
+# rm -f $PM_FILE
 
-./concurrent_pmdk/btree_concurrent -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 
+OUTPUT=fast-fair-pmdk-trace-btree_concurrent-$(date +"%Y-%m-%d_%H-%M").txt
+echo [*] Executing btree_concurrent '       ' [$OUTPUT]
+./concurrent_pmdk/btree_concurrent -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+
+OUTPUT=fast-fair-pmdk-trace-btree_concurrent_mixed-$(date +"%Y-%m-%d_%H-%M").txt
+echo [*] Executing btree_concurrent_mixed ' ' [$OUTPUT]
+./concurrent_pmdk/btree_concurrent_mixed -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
