9d8880fb94d4d487bfaa373c3877da3a5a4334e9
Ron Gatenio <ron.gatenio@gmail.com>
2024-03-19 05:09:01 +0200

    updated scripts

--- compile_pmdk.sh
+++ compile_pmdk.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 (cd concurrent_pmdk && make)
+
+if [ ! -z "$CPRD_RESULTS_BIN" ]; then
+    cp btree_concurrent* $CPRD_RESULTS_BIN
+fi
--- run_pmdk.sh
+++ run_pmdk.sh
@@ -1,17 +1,33 @@
 #!/bin/sh
 
-PM_FILE="/tmp/btree_data"
+PM_FILE="/tmp/pmem_btree_data"
 INPUT_FILE="sample_input.txt"
+NKEYS=10000
 NKEYS=1000
-NKEYS=11
 NTHREADS=4
 
-# rm -f $PM_FILE
 
-OUTPUT=fast-fair-pmdk-trace-btree_concurrent-$(date +"%Y-%m-%d_%H-%M").txt
-echo [*] Executing btree_concurrent '       ' [$OUTPUT]
-./concurrent_pmdk/btree_concurrent -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+rm -f $PM_FILE
 
-OUTPUT=fast-fair-pmdk-trace-btree_concurrent_mixed-$(date +"%Y-%m-%d_%H-%M").txt
-echo [*] Executing btree_concurrent_mixed ' ' [$OUTPUT]
-./concurrent_pmdk/btree_concurrent_mixed -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+PROG=btree_concurrent
+
+OUTPUT=fast-fair-pmdk.trace
+echo [*] Executing $PROG '             ' [$OUTPUT]
+./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+
+# OUTPUT=fast-fair-pmdk-recovery.trace
+# echo [*] Executing $PROG again '       ' [$OUTPUT]
+# ./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+
+
+rm -f $PM_FILE
+
+PROG=btree_concurrent_mixed
+
+OUTPUT=fast-fair-pmdk-mixed.trace
+echo [*] Executing $PROG '             ' [$OUTPUT]
+./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+
+# OUTPUT=fast-fair-pmdk-mixed-recovery.trace
+# echo [*] Executing $PROG again '       ' [$OUTPUT]
+# ./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
