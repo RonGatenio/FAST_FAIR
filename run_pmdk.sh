8b818eef5f5bf7b2b5dc9536d8af49bc16ad31ae
Ron Gatenio <ron.gatenio@gmail.com>
2024-04-03 13:37:35 +0300

    copy trace output

--- run_pmdk.sh
+++ run_pmdk.sh
@@ -2,7 +2,6 @@
 
 PM_FILE="/tmp/pmem_btree_data"
 INPUT_FILE="sample_input.txt"
-NKEYS=10000
 NKEYS=1000
 NTHREADS=4
 
@@ -13,11 +12,14 @@ PROG=btree_concurrent
 
 OUTPUT=fast-fair-pmdk.trace
 echo [*] Executing $PROG '             ' [$OUTPUT]
+# gdb --args ./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE
 ./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+cp $OUTPUT ${CPRD_RESULTS_TRACES}
 
 # OUTPUT=fast-fair-pmdk-recovery.trace
 # echo [*] Executing $PROG again '       ' [$OUTPUT]
 # ./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+cp $OUTPUT ${CPRD_RESULTS_TRACES}
 
 
 rm -f $PM_FILE
@@ -27,7 +29,9 @@ PROG=btree_concurrent_mixed
 OUTPUT=fast-fair-pmdk-mixed.trace
 echo [*] Executing $PROG '             ' [$OUTPUT]
 ./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+cp $OUTPUT ${CPRD_RESULTS_TRACES}
 
 # OUTPUT=fast-fair-pmdk-mixed-recovery.trace
 # echo [*] Executing $PROG again '       ' [$OUTPUT]
 # ./concurrent_pmdk/$PROG -n $NKEYS -t $NTHREADS -i $INPUT_FILE -p $PM_FILE 2> $OUTPUT
+cp $OUTPUT ${CPRD_RESULTS_TRACES}
